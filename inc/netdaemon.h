19c10e47dc95a0f7b6dc057bc1655d280c5bb5d1
Vadym Furmanchuk <furmanchuk.v.y@gmail.com>
2020-07-15 23:08:00 +0300

    refactoring

--- inc/netdaemon.h
+++ inc/netdaemon.h
@@ -1,9 +1,17 @@
-#include "argparser.h"
 #include <stdbool.h>
+
+
+struct dargs {
+    long long period;
+    long long rotate;
+    char *netinterface;
+    long long limMiB;
+    char *commandstr;
+};
 
 /**
  * daemod_run() - printing collect data in terminal from database in table
  * @args: :c:type:`_arguments`
  * @return: true -- if parse was successful or false -- otherwise
  */
-bool daemod_run(arguments *args);
+bool daemod_run(char *dbpath, struct dargs *dargs);
